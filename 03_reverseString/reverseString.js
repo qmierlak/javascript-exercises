d12784f6213524ae41e3baf025d64e35c233a662
Quinn <qmierlak@gmail.com>
2024-07-17 09:16:24 -0500

    Complete ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    if (string.length == 0) {
+        return "";
+    }
+
+    let reverse = [string.length];
+    let pos = 0;
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverse[pos] = string.at(i);
+
+        pos++;
+    }
+    // returns string without commas
+    return reverse.join("");
 };
 
 // Do not edit below this line
