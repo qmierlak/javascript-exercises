679d009f6badbe4df307969f74684f741cf57a58
Quinn <qmierlak@gmail.com>
2024-07-18 10:50:30 -0500

    Complete ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    let sum = 0;
+    let larger = val2;
+    let smaller = val1;
 
+    if(!(Number.isInteger(val1) && Number.isInteger(val2))) {
+        return "ERROR";
+    }
+    
+    if(val1 < 0 || val2 < 0) {
+        return "ERROR";
+    }
+
+
+    if(val1 > val2) {
+        larger = val1;
+        smaller = val2
+    }
+
+    for (let i=smaller; i<=larger; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
