371d9936cddea2cde103bb14f28a3634bbf90d49
Quinn <qmierlak@gmail.com>
2024-07-18 11:04:40 -0500

    Complete ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(temp) {
+  // from celsius
+  return Math.round((temp * 9/5 + 32)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+
+  // from farehnheit
+  return Math.round(((temp-32)*5/9)*10)/10;
 };
 
 // Do not edit below this line
