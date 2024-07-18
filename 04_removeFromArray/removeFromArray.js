62688b6dd8674a66e07300d229fd39adc130d570
Quinn <qmierlak@gmail.com>
2024-07-18 10:41:08 -0500

    Complete ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    // search through input array and check if that element is in the removal array
+
+    for(let i = 0; i < arr.length; i++) {
+        
+        if(args.includes(arr[i])) {
+            // Remove the element from the initial array
+            arr.splice(i,1);
+            i--;
+        }
+    
+    }
+    return arr;
+
 };
 
 // Do not edit below this line
